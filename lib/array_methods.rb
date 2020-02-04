3f26425184b565630b31818434bad61f45ee2250
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-04 02:30:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  max_value = 0 
+  max_value = array[0] 
   array.length.times{|index|
     if array[index] > max_value
       max_value = array[index]
@@ -13,7 +13,7 @@ return max_value
 end
 
 def find_min_value(array)
-  min_value = nil  
+  min_value = array[0]  
   array.length.times{|index|
     if array[index] < min_value
       min_value = array[index]
