00f20133d58b8dd3ecbf98266b606578170cb2b3
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-04 02:29:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,7 +5,7 @@ end
 def find_max_value(array)
   max_value = 0 
   array.length.times{|index|
-    if max_value == 0 || array[index] > max_value
+    if array[index] > max_value
       max_value = array[index]
     end 
  }
